46ab77241b32f41fd4bc37c31d79a575f9575494
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-05-08 11:16:24 +0100

    Tests MoveBlindForward()

--- main.cpp
+++ main.cpp
@@ -19,5 +19,11 @@ int main()
 
     std::cout << "Deck size after reset: " << table->GetDeckSize() << std::endl;
 
+    std::cout << "Player order before moving blind forward : " << std::endl;
+    table->PrintPlayerNumbers();
+    table->MoveBlindForward();
+    std::cout << "Player order after" << std::endl;
+    table->PrintPlayerNumbers();
+
     return 0;
 }
